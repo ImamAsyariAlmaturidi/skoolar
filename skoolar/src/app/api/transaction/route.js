044ef3d582f8b30d4f3a479fbffadb39a093d12b
ImamAsyariAlmaturidi <imamasyari700@gmail.com>
2024-09-08 17:49:19 +0700

    midtrans

--- skoolar/src/app/api/transaction/route.js
+++ skoolar/src/app/api/transaction/route.js
@@ -1,43 +1,37 @@
-// import { NextResponse } from "next/server";
-// export async function POST() {
-//   try {
-//   } catch (error) {
-//     return NextResponse.json({
-//       statusCode: 500,
-//       message: "Internal server error",
-//       data: [],
-//     });
-//   }
-// }
-// export async function GET() {
-//   try {
-//   } catch (error) {
-//     return NextResponse.json({
-//       statusCode: 500,
-//       message: "Internal server error",
-//       data: [],
-//     });
-//   }
-// }
-// export async function PUT() {
-//   try {
-//   } catch (error) {
-//     return NextResponse.json({
-//       statusCode: 500,
-//       message: "Internal server error",
-//       data: [],
-//     });
-//   }
-// }
-// export async function DELETE() {
-//   try {
-//   } catch (error) {
-//     return NextResponse.json({
-//       statusCode: 500,
-//       message: "Internal server error",
-//       data: [],
-//     });
-//   }
-// }
+import Midtrans from "midtrans-client";
 
-// export default Transaction;
+// import { NextResponse } from "next-server";
+
+let snap = new Midtrans.Snap({
+  isProduction: false,
+  serverKey: process.env.SERVER_KEY_MIDTRANS,
+  clientKey: process.env.CLIENT_KEY_MIDTRANS,
+});
+
+export async function POST(request) {
+  const { id, productName, price, quantity } = await request.json();
+
+  let parameter = {
+    item_details: {
+      name: productName,
+      price: price,
+      quantity: quantity,
+    },
+    transaction_details: {
+      order_id: id,
+      gross_amount: price * quantity,
+    },
+  };
+
+  const token = await snap.createTransactionToken(parameter);
+
+  console.log(token);
+
+  return Response.json({
+    token,
+  });
+}
+
+export async function PATCH(order_id) {
+  const { id, productName, price, quantity } = await request.json();
+}
