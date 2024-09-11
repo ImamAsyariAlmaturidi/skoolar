a682b8976351047a77da78b9367c1333430e4643
halofarhan <farhanamirullh@gmail.com>
2024-09-11 15:20:26 +0700

    done loading

---
+++ skoolar/src/app/dashboard/parent/loading.jsx
@@ -0,0 +1,11 @@
+export default function loadingPage() {
+
+    return (
+        <>
+            <div className="h-screen w-screen flex justify-center items-center bg-white">
+                <img className="h-24" src="https://res.cloudinary.com/dqrmcom6v/image/upload/v1725532172/SKOOLAR_trw2yi.png" alt="" />
+            </div>
+        </>
+    )
+
+}
