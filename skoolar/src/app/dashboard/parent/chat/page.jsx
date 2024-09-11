d458b84ec1d54426f9594a40cbc2d37b31c59eb6
Dylboy <firmantodylan@gmail.com>
2024-09-12 02:50:50 +0700

    step3: deploy

--- skoolar/src/app/dashboard/parent/chat/page.jsx
+++ skoolar/src/app/dashboard/parent/chat/page.jsx
@@ -1,17 +1,34 @@
+"use client";
+import { useEffect, useState } from "react";
 import Link from "next/link";
-import ChatRoom from "../../../../components/parent/ChatRoom";
 import ChatBox from "../../../../components/parent/ChatBox";
 import SideBar from "../../../../components/parent/Sidebar";
 import { getAllGroup } from "./action";
-export default async function ChatPage() {
-  const { data } = await getAllGroup();
+
+export default function ChatPage() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Memfetch data dari API saat komponen mount
+    const fetchData = async () => {
+      try {
+        const result = await getAllGroup();
+        setData(result.data || []); // Mengupdate state dengan data yang difetch
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    };
+
+    fetchData(); // Memanggil fungsi fetch
+  }, []); // [] artinya efek ini hanya akan dijalankan sekali, saat komponen mount
+
   return (
     <>
       <div className="w-full flex gap-3 px-5 py-10 h-screen bg-[#F1F7FE]">
         <SideBar />
         <div className="flex w-full">
-          <div className=" bg-white w-[30rem]  rounded-2xl  rounded-r-none border-r border-neutral-200 border-solid">
-            <div className="border-b  border-neutral-200 flex justify-start items-center h-[11%]">
+          <div className="bg-white w-[30rem] rounded-2xl rounded-r-none border-r border-neutral-200 border-solid">
+            <div className="border-b border-neutral-200 flex justify-start items-center h-[11%]">
               <p className="ml-8 text-[#3166ec] font-semibold text-2xl">
                 Message
               </p>
@@ -21,15 +38,14 @@ export default async function ChatPage() {
               <p className="ml-8 mt-3 mb-2 text-xs text-neutral-400">
                 Group chat
               </p>
-              {data?.map((groups) => {
-                return (
-                  <>
-                    <Link href={`/dashboard/parent/chat/${groups._id}`}>
-                      <ChatBox data={groups} />
-                    </Link>
-                  </>
-                );
-              })}
+              {data.map((group) => (
+                <Link
+                  key={group._id}
+                  href={`/dashboard/parent/chat/${group._id}`}
+                >
+                  <ChatBox data={group} />
+                </Link>
+              ))}
             </div>
             {/* <div>
               <p className="ml-8 mt-7 mb-2 text-xs text-neutral-400">
